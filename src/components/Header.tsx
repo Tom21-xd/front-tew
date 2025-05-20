1784c584656b99b5c3a5606e7e149faba1d3bf50
SeitoUwU <brayan.gomez@udla.edu.co>
2025-05-20 11:35:15 -0500

    ✨ feat(reportes): añadir nueva página de reportes con componentes de aeropuertos y actualizar dependencias 🛫
    🧩 Crea nueva página de reportes para visualización de información aeroportuaria 📊
    🛫 Implementa componentes específicos:
    
    CardAirports para mostrar información de aeropuertos 🏢
    OptionAirports para opciones de selección ✅
    
    🔄 Actualiza enlace en el encabezado para dirigir a la nueva página de reportes 🔗
    🎨 Añade nuevos componentes UI para etiquetas y grupos de radio buttons 🔘
    📦 Incluye dependencias necesarias en package.json 📚
    🗑️ Elimina archivo obsoleto main.tsx 🧹

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const links = [
   { name: "Inicio", href: "/" },
-  { name: "dashboard", href: "/viajes" },
+  { name: "dashboard", href: "/reports" },
 ];
 
 export default function Header() {
