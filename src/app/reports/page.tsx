97bdff75a128c9c33724c7b0a09a9952c04f67c9
SeitoUwU <brayan.gomez@udla.edu.co>
2025-05-20 21:38:16 -0500

    style: add placeholder text for layout testing
    This commit adds the text "Holi" as a placeholder to test the layout structure of the reports page. This change helps in verifying the alignment and spacing of components during development.

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -5,6 +5,7 @@ import OptionAirports from '@/components/reports/OptionAirports';
 export default function Page(){
     return(
         <div className="flex flex-col h-screen overflow-auto pt-16 pb-8">
+            Holi
             <div className="flex-none">
                 <OptionAirports />
             </div>
